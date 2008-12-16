eef3a436b24443583e72ffd149e17615d5b19448
Arne Wichmann <arnew@rasentrimmer.org>
2008-12-16 15:07:29 +0100

    rename

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,3 +1,4 @@
+#include <math.h>
 
 int main ( void ) {
 	int x,y,N=30; 
@@ -5,8 +6,10 @@ int main ( void ) {
 	for(x=0;x<N;x++){
 		for(y=0;y<2*N;y++) {
 			int r = 1000;
-			complex zi = 0, c = (2.0*y)/(2*N)-1.0 + I*((2.0*x)/N-1.0);
-			while (--r>0 && cabs(zi = zi * zi + c) < 1.0);
+			double z = 0, zi = 0, c = (2.0*y)/(2*N)-1.0, ci= I*((2.0*x)/N-1.0);
+			while (--r>0 && sqrt(z*z+zi*zi) < 1.0) {
+				;
+		}
 			putchar(r?' ':'X');
 		}
 		putchar('\n');
