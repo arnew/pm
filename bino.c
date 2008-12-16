5eb2240fb360c3ac374a9c8798d62e3f9004a707
Arne Wichmann <arnew@rasentrimmer.org>
2008-12-16 08:59:18 +0100

    und nochn baum

--- bino.c
+++ bino.c
@@ -6,8 +6,8 @@
                                          
                                        #include\
                                        <stdio.h>
-                                     #include    \
-                                       <stdlib.h>
+                                     #include/**/\
+                                    /**/ <stdlib.h>
                                    int  main  (  int 
                                   c, char**v){ static 
                                  int n_=0, /* Merry X-
