158ae8d9f13a5117f249de84a9e859214f97c026
Arne Wichmann <arnew@rasentrimmer.org>
2009-08-23 16:35:32 +0200

    tree3 -> tree1

--- tree1.c
+++ tree1.c
@@ -1,26 +1,37 @@
 
-                    const 
+#include <stdio.h>
+#include <stdlib.h>
 
-                     int 
-                    N=20;
-                    
-                  #include\
-                  <stdio.h>
-                #include/**/\
-               /**/ <stdlib.h>
-              int  main  (  int 
-             c, char**v){ static 
-            int n_=0, /* Merry X-
-           Mas */k_=0,*a;int n=n_,
-          k=k_,p=10;if(v){a=calloc(
-         (2+N)*(N+1),sizeof(int)/2);
-        #define X ( a+ (n*n + n)/2+k)
-      while(n<=N){while(k++<N-n)putchar
-     (' ');k=n;do printf("%i ",main((n_=
-    n,k_=k),0));while(k--);n++;k=0;printf
-   ("\n");}free(a);}return v?c:(*X=(k&&k!=
-  n)?*X?*X:(main((n_=n-1,k_=k),0)+main((n_=
-                   n-1,k_=
-                   k-1),0)
-                   )%p:1);}
+#define t char
+const t M = 10, Pmod=2;
 
+int main (int c, char **v)
+{
+#define n x(**v,t,0,1)
+#define k x(**v,t,0,2)
+#define N x(c,t,0,0)
+#define K x(c,t,0,1)
+#define x(p,t,O,o) (*(((t*)(((char*)(&(p)))+(O)))+(o)))
+#define g(n) (((n)*(n)+(n))/2)
+	N = n;
+	K = k;
+#define m x(**v,t,0,0)
+	if (m) {
+		m=N=0;
+#define a x(**v,t*,3*sizeof(t),0)
+		a = calloc (g(M+1), sizeof (t));
+		while (N <= M)
+		{
+			K = 0;
+			while (K++ < M - N) putchar (' ');
+			K = N;
+			do printf ("%i ", main ((n = N, k = K), v)); while (K--);
+			N++;
+			printf ("\n");
+		}
+		free (a);
+	} else
+#define X x(*a,t,0,g(N)+K)
+	return X = (K && K != N) ?  X ?  X : (main ((n = N - 1, k = K), v) + main ((n = N - 1, k = K - 1), v)) % Pmod : 1;
+	return 0;
+}
