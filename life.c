2dbba7ebf59d9ea0798e53d60f429def7a6eaefb
Arne Wichmann <arnew@rasentrimmer.org>
2010-11-24 21:18:31 +0100

    ...

--- life.c
+++ life.c
@@ -1,4 +1,7 @@
-const int N=20,M=80;
+#include<stdio.h>
+#include<assert.h>
+
+const int N=32,M=126;
 #define d (b-a)
 #define s ( d<M+2 || d>=(N+1)*(M+2) || d%(M+2)==0 || (d+1)%(M+2)==0)
 
@@ -15,6 +18,7 @@ void p(char*a) {
 int main(int ac, char**av) {
 	unsigned char a[(N+2)*(M+2)];
 	unsigned char*b=a;
+	unsigned int *o,*oo,*ooo;
 	unsigned long int g=0;
 	char c=1;
 
@@ -23,29 +27,41 @@ int main(int ac, char**av) {
 		b++;
 	}
 
+	/*
+	*(a+250)=1;
+	*(a+251)=1;
+	*(a+252)=1;
+	*/
+
 	
 	while(c && g<10000) {
 		/*
 		p(a);
+		getchar();
+			*(a+((d+2*(M+2))%((M+2)*(N+2)))) <<=1;
 		*/
 		c=0;
 		for(b=a;d<(N+2)*(M+2);b++) *b<<=1;
 
 		for(b=a;d<(N+2)*(M+2);b++) {
 			if (s) continue;
-			int n =   ((*(b-1-M-2))&0x2)/2 
-				+ ((*(b  -M-2))&0x2)/2 
-				+ ((*(b+1-M-2))&0x2)/2 
-				+ ((*(b-1    ))&0x2)/2 
-				+ ((*(b+1    ))&0x2)/2 
-				+ ((*(b-1+M+2))&0x2)/2 
-				+ ((*(b  +M+2))&0x2)/2 
-				+ ((*(b+1+M+2))&0x2)/2;
+			o=(void*)(b-1-M-2);
+			oo=(void*)(b-1);
+			ooo=(void*)(b-1+M+2);
+			int n = ((*(o))&0x020202)+
+				((*(oo))&0x020002)+
+				((*(ooo))&0x020202);
+
+			n =       ((n&0xff  )>>1)
+				+ ((n&0xff<<8)>>9 )
+				+ ((n&0xff<<16)>>17);
 		       
 			switch(n) {
 				case 2: 
 					*b+=(*(b)%4)/2;
 					break;
+			}
+			switch(n) {
 				case 3:
 					c++;
 					*b+=1;
